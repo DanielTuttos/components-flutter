c8dc3788aaa2a0b971c34494b6698884a98a552a
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-01-12 13:16:15 -0500

    :construction: widget listView

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Componentes APP',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Componentes'),
-        ),
-        body: const HomePageTemp(),
-      ),
+      home: HomePageTemp(),
     );
   }
 }
--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -3,10 +3,50 @@ import 'package:flutter/material.dart';
 class HomePageTemp extends StatelessWidget {
   const HomePageTemp({super.key});
 
+  final options = const ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis'];
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemsCorta(),
+        // children: _crearItems(),
+      ),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = <Widget>[];
+    for (String opt in options) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista
+        ..add(tempWidget)
+        ..add(const Divider());
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return options.map((String item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item),
+            subtitle: const Text('Hola que tal'),
+            leading: const Icon(Icons.balance),
+            trailing: const Icon(Icons.arrow_right),
+            onTap: () {
+              print(item);
+            },
+          ),
+          const Divider(),
+        ],
+      );
+    }).toList();
   }
 }
