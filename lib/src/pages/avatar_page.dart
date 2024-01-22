80b13b7ece4c4cc329e3c46e2554c27461658ad7
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-01-22 15:39:29 -0500

    :construction: avatar page finish

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -8,6 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(5.0),
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media.licdn.com/dms/image/D5635AQGN3JXH0BcEOA/profile-framedphoto-shrink_100_100/0/1696717785080?e=1706562000&v=beta&t=9Gr0gWgNCijcPWz8iqbxdo772ZcF8ESDuylZ-HjVZuo'),
+              radius: 30.0,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: const CircleAvatar(
+              backgroundColor: Colors.brown,
+              child: Text('DR'),
+            ),
+          )
+        ],
+      ),
+      body: const Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage(
+              'https://img.freepik.com/foto-gratis/pico-montana-nevada-majestuosidad-galaxia-estrellada-ia-generativa_188544-9650.jpg'),
+          fadeInDuration: Duration(milliseconds: 300),
+        ),
       ),
     );
   }
