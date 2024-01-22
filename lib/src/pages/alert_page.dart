70d085e9c68db4eac7b601d673bd73501adfed6e
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-01-22 15:18:55 -0500

    :construction: alert page finish

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -9,12 +9,54 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Alert Page'),
       ),
+      body: Center(
+        child: TextButton(
+          onPressed: () => _showAlert(context),
+          style: const ButtonStyle(
+            backgroundColor: MaterialStatePropertyAll<Color>(Colors.blue),
+          ),
+          child: const Text('Mostrar Alerta',
+              style: TextStyle(color: Colors.white, fontSize: 20.0)),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.arrow_left),
+        child: const Icon(Icons.arrow_left_outlined),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0)),
+            title: const Text('titulo'),
+            content: const Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('contenido de la caja de la alerta'),
+                FlutterLogo(
+                  size: 100.0,
+                )
+              ],
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text('CANCELAR')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'))
+            ],
+          );
+        });
   }
 }
